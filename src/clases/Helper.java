f5e8656b48131d2657575e9accbfa758bb0f2578
lenninlara28 <lenninlara28@hotmail.com>
2016-10-21 08:10:28 -0500

    Primer Avance

---
+++ src/clases/Helper.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author hp 14
+ */
+public class Helper {
+
+    public static void mensaje(Component ventana, String mensaje, int tipo) {
+        switch (tipo) {
+            case 1:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Informacion", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Advertencia", JOptionPane.WARNING_MESSAGE);
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
+                break;
+        }
+    }
+
+    public static void limpiadoTabla(JTable tabla1) {
+        int nf, nc;
+
+        nf = tabla1.getRowCount();
+        nc = tabla1.getColumnCount();
+        for (int i = 0; i < nf; i++) {
+            for (int j = 0; j < nc; j++) {
+                tabla1.setValueAt("", i, j);
+
+            }
+        }
+    }
+
+    public static void porDefectoTabla(JTable tabla1) {
+        DefaultTableModel tm;
+        tm = (DefaultTableModel) tabla1.getModel();
+        tm.setColumnCount(0);
+        tm.setRowCount(0);
+
+    }
+
+    public static void habilitarBotones(JButton[] botonesH) {
+        for (int i = 0; i < botonesH.length; i++) {
+            botonesH[i].setEnabled(true);
+
+        }
+    }
+
+    public static void deshabilitarBotones(JButton[] botonesD) {
+        for (int i = 0; i < botonesD.length; i++) {
+            botonesD[i].setEnabled(false);
+
+        }
+    }
+    
+    public static void habilitarMesas(JRadioButton[] RadiosH){
+        for (int i = 0; i < RadiosH.length; i++) {
+             if (RadiosH[i].isSelected()){
+                 
+             }
+            
+        }
+        
+    }
+
+}
